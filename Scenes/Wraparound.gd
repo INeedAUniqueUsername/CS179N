d852245c56a3718b06240b07cc195e4b664d694c
Alex Chen <achen115@ucr.edu>
2022-04-15 16:56:42 -0700

    add wraparound + begin LaserSentry

--- Scenes/Wraparound.gd
+++ Scenes/Wraparound.gd
@@ -4,15 +4,54 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-var limit = 300
-
+var radius = 300
+onready var diameter = radius * 2
 var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	call_deferred("register_actors")
+var player
+var leaves
+func register_actors():
+	leaves = []
+	var d = get_descendants(self)
+	for l in d:
+		if l.is_in_group("Actor"):
+			leaves.append(l)
+			if l.is_in_group("Player"):
+				player = l
+	if player == null:
+		pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	time += delta
-	if time > 1:
+	if time > 0.5:
 		time = 0
+		var pl = player.global_position
+		for l in leaves:
+			var offset = l.global_position - pl
+			while offset.x < -radius:
+				offset.x += diameter
+				l.global_position.x += diameter
+			while offset.x > radius:
+				offset.x -= diameter
+				l.global_position.x -= diameter
+			while offset.y < -radius:
+				offset.y += diameter
+				l.global_position.y += diameter
+			while offset.y > radius:
+				offset.y -= diameter
+				l.global_position.y -= diameter
 		#to do: iterate through all children and implement wraparound
+func get_leaves(n):
+	if n.get_child_count() == 0:
+		return [n]
+	var c = []
+	for ch in n.get_children():
+		c.append_array(get_leaves(ch))
+	return c
+func get_descendants(n):
+	var c = [n]
+	for ch in n.get_children():
+		c.append_array(get_descendants(ch))
+	return c
---
+++ SentryGun.gd
@@ -0,0 +1,22 @@
+extends Sprite
+
+var target
+func _on_area_entered(area):
+	var p = area.get_parent()
+	if p.is_in_group("Player"):
+		target = p
+func _process(delta):
+	if target != null:
+		# FIX ANGLE BUG
+		var offset = target.global_position - global_position
+		offset = offset.angle() - rotation
+		if offset > PI:
+			offset = 2 * PI - offset
+		if offset < -PI:
+			offset += PI * 2
+			
+		var speed = PI / 90
+		offset = offset / 30
+		if abs(offset) > speed:
+			offset = sign(offset) * speed
+		rotation += offset
