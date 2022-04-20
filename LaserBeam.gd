a07db0462df368f0926beba719f89dddf3c0ae8b
Patrick Dang <patrickdangt@gmail.com>
2022-04-20 09:16:03 -0700

    Added a damage function for laserbeam

--- LaserBeam.gd
+++ LaserBeam.gd
@@ -1,7 +1,14 @@
 extends Node2D
 var vel = Vector2(0, 0)
 export(float) var damp = 1
+
+var dmg = 10
+
 func _physics_process(delta):
 	if damp > 0:
 		vel -= vel.normalized() * damp * delta
 	global_translate(vel * delta)
+
+func _on_Area2D_area_entered(area):
+	area.get_parent().get_parent().take_damage(dmg)
+	queue_free()
