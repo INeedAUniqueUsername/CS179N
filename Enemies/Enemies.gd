c03d6007f882db8f764b0aa41d21cd4e87dfc129
Patrick Dang <patrickdangt@gmail.com>
2022-04-28 23:15:29 -0700

    Updated Laser Drone

--- Enemies/Enemies.gd
+++ Enemies/Enemies.gd
@@ -9,20 +9,26 @@ func get_hp(): return hp
 
 func takeDamage(dmg: int): hp -= dmg
 
-#func _on_Damage_Area_area_entered(area):
-#	var hit = area.get_parent()
-#	if hit.is_in_group("projectile"):
-#		takeDamage(hit.dmg)
-#		area.get_parent().queue_free()
+func _on_Damage_Area_area_entered(area):
+	var hit = area.get_parent()
+	if hit.is_in_group("projectile"):
+		takeDamage(hit.dmg)
+		area.get_parent().queue_free()
 
 # Triggered when player enters enemy's detection radius
-func _on_Detect_Area_area_entered(body):
-	if body.is_in_group("Player"):
-		target = body
+func _on_Detect_Area_area_entered(area):
+	if !Helper.is_area_body(area):
+		return
+	var actor = Helper.get_parent_actor(area)
+	if actor and actor.is_in_group("Player"):
+		target = actor
 
 # Triggered when player exits enemy's detection radius 
-func _on_Detect_Area_area_exited(body):
-	if body.is_in_group("Player"):
+func _on_Detect_Area_area_exited(area):
+	if !Helper.is_area_body(area):
+		return
+	var actor = Helper.get_parent_actor(area)
+	if actor and actor.is_in_group("Player"):
 		target = null
 
 # Called every frame to constantly look at the target
