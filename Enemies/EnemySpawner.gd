3abe6dadb6587deeff286ae7ff1f6598f006eb66
Yunjie Fang <yfang038@ucr.edu>
2022-04-29 16:22:04 -0700

    update enemy spawner

--- Enemies/EnemySpawner.gd
+++ Enemies/EnemySpawner.gd
@@ -1,17 +1,17 @@
 extends Node
 
-func _on_Spawn_timer_timeout():
+func _on_random_generation_timer_timeout():
 	var rand = RandomNumberGenerator.new()
 	var enemyscene = [
 		load("res://Enemies/LaserDrone.tscn")
 	]
 	var screen_size = get_viewport().get_visible_rect().size
-	for i in range(0,1):
-		var enemy = enemyscene[randi() % enemyscene.size()].instance()
-		rand.randomize()
-		var x = rand.randf_range(0, screen_size.x)
-		rand.randomize()
-		var y = rand.randf_range(0, screen_size.y)
-		enemy.position.y = y
-		enemy.position.x = x
-		add_child(enemy)
+	
+	var enemy = enemyscene[randi() % enemyscene.size()].instance()
+	rand.randomize()		
+	var x = rand.randf_range(0, screen_size.x)
+	rand.randomize()
+	var y = rand.randf_range(0, screen_size.y)
+	enemy.position.y = y
+	enemy.position.x = x
+	add_child(enemy)
