2dd7c012bfaebcc58463ddb2c9a885a23b34b841
Alex Chen <achen115@ucr.edu>
2022-05-06 16:17:18 -0700

    tweak Lightning Drone

--- Enemies/Lightning Drone.gd
+++ Enemies/Lightning Drone.gd
@@ -1,52 +1,45 @@
 extends Enemies
-
-
 var vel = Vector2(0, 0)
-
-
 var time_scale = 1.0
 func set_time_scale(t:float):
 	time_scale = t
 	$Anim.playback_speed = t
-
-
-var patrol_time = 10
-var curr_patrol_time = 0
 func _physics_process(delta):
 	delta *= time_scale
 	if target:
+		print('target: ' + target.name)
 		var speed = 180 * delta
-		var offset = (target.global_position - global_position)
-		vel -= vel.normalized() * speed / 2
-		
-		if offset.length_squared() > 180 * 180:
-			vel += offset.rotated(PI/4).normalized() * speed
-		else:
-			vel += offset.rotated(PI/2).normalized() * speed
-		
-	elif target == null:
-		curr_patrol_time -= delta
-		if curr_patrol_time < 0:
-			curr_patrol_time = patrol_time
-			rotate(PI/2)
-		
+		var offset = target.global_position - global_position
+		if $Anim.current_animation == "Wait":
+			vel -= vel.normalized() * speed / 2
+			if offset.length_squared() > 180 * 180:
+				vel += offset.rotated(PI/4).normalized() * speed
+			else:
+				vel += offset.rotated(PI/2).normalized() * speed
+		elif $Anim.current_animation == "Charge":
+			
+			var rejection = vel * (1 - vel.normalized().dot(offset.normalized()))
+			vel -= rejection.normalized() * min(rejection.length(), speed)
+			vel += offset.normalized() * speed
 	global_translate(vel * delta)
-
 func _ready():
 	hp = 50
-	$Anim.play("wait")
 	$Anim.connect("animation_finished", self, "_on_animation_finished")
-
-
+var salvo = 0
 func _on_animation_finished(name):
-	if name == "wait":
-		$Anim.play("wait2")
-		if atk_target != null:
-			fire_salvo_1()
-	if name == "wait2":
-		$Anim.play("wait")
-		if atk_target != null:
-			fire_salvo_1()
+	if name == "Wait":
+		if target:
+			$Anim.play("Charge")
+		else:
+			$Anim.play("Wait")
+	if name == "Charge":
+		$Anim.play("Wait")
+		if target:
+			salvo += 1
+			if salvo % 2 == 0:
+				fire_salvo_1()
+			else:
+				fire_salvo_2()
 var beam = preload("res://LightningBeam.tscn")
 func fire_salvo_1():
 	var ignore = [self]
