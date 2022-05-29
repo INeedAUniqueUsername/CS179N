1f8d4adb8003836d009744af2f4534892c46512c
Alex Chen <achen115@ucr.edu>
2022-05-28 22:49:26 -0700

    tweak LaserSentry and JunkDrone

--- Enemies/JunkDrone.gd
+++ Enemies/JunkDrone.gd
@@ -1,6 +1,5 @@
 extends Node2D
 const score = 50
-var moveSpeed = 70
 var rotationSpeed = 0.5
 var vel = Vector2(0, 0)
 
@@ -8,7 +7,6 @@ var hp = 100 setget, get_hp
 func get_hp(): return hp
 
 var damage = 10
-var rng = RandomNumberGenerator.new()
 
 var is_broken = false
 
@@ -22,18 +20,18 @@ func _ready():
 	elif PlayerVariables.difficulty == 2:
 		hp = 150
 	# Start out at a random direction
-	rng.randomize()
-	var x = rng.randf_range(-1, 1)
-	var y = rng.randf_range(-1, 1)
-	vel = Vector2(x, y)
+	vel = polar2cartesian(70, randf() * PI*2)
 	
 func _physics_process(delta):
-	global_translate(vel.normalized() * moveSpeed * delta)
+	global_translate(vel * delta)
 	rotation += delta * rotationSpeed
 
 signal on_destroyed
 func damage(projectile):
-	hp -= projectile.damage
+	var dmg = projectile.damage
+	if projectile.is_in_group("Laser") or projectile.is_in_group("Lightning"):
+		dmg *= 0.4
+	hp -= dmg
 	if hp < 1:
 		emit_signal("on_destroyed", self)
 		var enemydie = AudioStreamPlayer.new()
@@ -54,30 +52,32 @@ func spawn_laserdrone():
 		var laserdrone_load = laserdrone.instance()
 		
 		get_parent().add_child(laserdrone_load)
-		rng.randomize()
-		var x = rng.randf_range(-50, 50)
-		laserdrone_load.global_position = global_position + Vector2(x, x)
-		laserdrone_load.rotation_degrees = rng.randf_range(0, 360)
-
+		laserdrone_load.global_position = global_position
+		laserdrone_load.rotation_degrees = rand_range(0, 360)
+		laserdrone_load.vel = polar2cartesian(120, randf() * PI * 2)
 func spawn_broken():
 	for i in range(3):
 		var broken_load = self.duplicate()
-		rng.randomize()
-		var x = rng.randf_range(-1, 1)
-		var y = rng.randf_range(-1, 1)
-		broken_load.vel = Vector2(x, y)
+		broken_load.vel = polar2cartesian(1, randf() * PI * 2)
 		broken_load.is_broken = true
 		
 		get_parent().add_child(broken_load)
-		broken_load.scale = Vector2(0.75, 0.75)
+		broken_load.scale = Vector2(0.5, 0.5)
 		broken_load.global_position = global_position
 
 func _on_Damage_Area_area_entered(area):
 	if !Helper.is_area_body(area):
 		return
 	var actor = Helper.get_parent_actor(area)
-	if actor and actor.is_in_group("Player"):
+	if !actor:
+		return
+	if actor.is_in_group("Player"):
 		var velDiff = self.vel - actor.common.vel
-		actor.common.vel += velDiff / 2
-		self.vel -= velDiff
+		actor.common.vel += velDiff
+		self.vel -= velDiff / 5.0
+		actor.damage(self)
+	elif actor.is_in_group("Actor") and !actor.is_in_group("Projectile"):
+		var velDiff = self.vel - actor.vel
+		actor.vel += velDiff
+		self.vel -= velDiff / 5.0
 		actor.damage(self)
